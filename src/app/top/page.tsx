f773d5cdbdc37d3e81dd1de5c513a6e8752ba6aa
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-09-29 23:50:08 +0900

    all

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,24 +1,26 @@
 import { fileURLToPath } from "url";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import Image from "next/image";
 import next from "next";
-interface HOMEProps{};
+interface HOMEProps {}
 import Link from "next/link";
 import Button from "@/component/button";
 
-const HOME=()=>{
-    return(
- <>
- <Image 
-         className={styles.Image}
-         width={300}
-         height={300}
-         alt="LOGO"
-         src={"window.svg"}
-         ></Image>
-<Button children="TAP" color="pink" link="/login"></Button>
- </>       
-    );
-}
+
+const HOME = () => {
+  return (
+    <>
+      <Link href="/home">
+        <Image
+          className={styles.Image}
+          width={300}
+          height={300}
+          alt="LOGO"
+          src={"globe.svg"}
+        ></Image>
+      </Link>
+    </>
+  );
+};
 
 export default HOME;
